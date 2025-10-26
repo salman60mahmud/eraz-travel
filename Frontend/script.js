ec03b7d7b522017523f21323aa0e06d1c9e5cfdf
salman60mahmud <salmanmahmud947.com>
2025-10-26 23:38:39 +0600

    selected seat section left

---
+++ Frontend/script.js
@@ -0,0 +1,10 @@
+let seat = 40;
+
+let available_seat = document.getElementById('available_seat');
+available_seat.innerText = `${seat} Seats available`;
+
+function select_seat(id){
+    console.log(id);
+    
+
+}
